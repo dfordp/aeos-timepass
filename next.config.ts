981ba8ed9516f3b85744d38aece61923e41e9553
Dilpreet <92905896+dfordp@users.noreply.github.com>
2025-05-20 20:22:14 +0530

    vercel skill issue

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['pub-9a5e5383cb80441e8aeb06ba0e4a7933.r2.dev'],
+    domains: ['pub-9a5e5383cb80441e8aeb06ba0e4a7933.r2.dev','pub-52d80ec62f4c4854ade3b5ef86bad061.r2.dev'],
   },
 };
 
